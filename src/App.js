90b5b9e56f47dd377745a7d23bb7fd60dc87d2e3
JanaAbdelbaky <124375273+JanaAbdelbaky@users.noreply.github.com>
2024-09-30 18:16:16 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // External variables
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 //Make sure to add your MongoDB URI in the .env file as MONGO_URI="your mongodb uri"
 //Check db connection links in README file
 const MongoURI = process.env.MONGO_URI;
 //const {createUser, getUsers, createBlog, filterBlog, editBlog}= require('./Routes/userController')
-
+const {
+  createTourismGoverner,
+  createAdmin,
+} = require("./Routes/adminController");
 
 //App variables
 const app = express();
@@ -14,20 +17,23 @@ const port = process.env.PORT || "8000";
 //const user = require('./Models/User');
 // #Importing the userController
 
-
 // configurations
 // Mongo DB
-mongoose.connect(MongoURI)
-.then(()=>{
-  console.log("MongoDB is now connected!")
-// Starting server
- app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+mongoose
+  .connect(MongoURI)
+  .then(() => {
+    console.log("MongoDB is now connected!");
+    // Starting server
+    app.listen(port, () => {
+      console.log(`Listening to requests on http://localhost:${port}`);
+    });
   })
-})
-.catch(err => console.log(err));
+  .catch((err) => console.log(err));
 app.get("/home", (req, res) => {
-    res.status(200).send("You have everything installed!");
-  });
+  res.status(200).send("You have everything installed!");
+});
 
-app.use(express.json())
+app.use(express.json());
+
+app.post("/tourism-governor", createTourismGoverner);
+app.post("/admin", createAdmin);
--- src/Routes/adminController.js
+++ src/Routes/adminController.js
@@ -1,29 +1,41 @@
+const TourismGoverner = require("../Models/TourismGoverner");
+const Admin = require("../Models/Admin");
+
 //Tourism Governer
 const createTourismGoverner = async (req, res) => {
   const { username, password } = req.body;
-  username = req.body.username;
+
   try {
-    const user = await userModel.create({
+    await TourismGoverner.create({
       username,
       password,
     });
     res.status(200).json({ msg: "Tourism Governer created" });
-  } catch {
-    res.status(400).json({ message: "Error retrieving users", error });
+  } catch (error) {
+    res
+      .status(400)
+      .json({
+        message: "Error creating Tourism Governer",
+        error: error.message || error,
+      });
   }
 };
 
 //Admin
 const createAdmin = async (req, res) => {
   const { username, password } = req.body;
-  username = req.body.username;
+
   try {
-    const user = await userModel.create({
+    await Admin.create({
       username,
       password,
     });
     res.status(200).json({ msg: "Admin created" });
-  } catch {
-    res.status(400).json({ message: "Error retrieving users", error });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating Admin", error: error.message || error });
   }
 };
+
+module.exports = { createTourismGoverner, createAdmin };
