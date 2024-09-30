fc29551b58b9eceb2eb55acbecfb400768f5f5f3
habibagaber <habibamohamed55874@gmail.com>
2024-09-30 21:13:27 +0300

    commit test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 // External variables
 const express = require("express");
 const mongoose = require('mongoose');
@@ -31,3 +32,40 @@ app.get("/home", (req, res) => {
   });
 
 app.use(express.json())
+*/
+// app.js
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const advertiserRoutes = require("./Routes/advertiserController"); // Adjust path as necessary
+
+// Load environment variables from .env file
+dotenv.config();
+
+// App variables
+const app = express();
+const port = process.env.PORT || 3000;
+const MongoURI = process.env.MONGO_URI;
+
+// Middleware
+app.use(express.json());
+app.use('/api', advertiserRoutes); // Use advertiser routes under '/api'
+
+// MongoDB Connection
+mongoose.connect(MongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB connected!");
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+})
+.catch(err => console.error(err));
+
+// Basic route for testing
+app.get("/home", (req, res) => {
+  res.status(200).send("You have everything installed!");
+});
