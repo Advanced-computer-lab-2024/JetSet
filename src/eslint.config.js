a7db809f33fedce888db501bb4cd9d6f41743635
JanaAbdelbaky <ja8834610@gmail.com>
2026-06-22 14:33:22 +0300

    add eslint config

---
+++ src/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // Node.js runs on server
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+];
