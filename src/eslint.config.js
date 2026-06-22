9af184695d4d9f74df42f1c39892e109d26b3ccd
JanaAbdelbaky <ja8834610@gmail.com>
2026-06-22 14:44:25 +0300

    Add CI pipeline

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,13 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      react,
+    },
     languageOptions: {
       globals: {
-        ...globals.node, // Node.js runs on server
+        ...globals.browser,
         ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
     rules: {
