945126a3e532657cb42345b40ddd3ad97f9c09fa
JanaAbdelbaky <ja8834610@gmail.com>
2026-06-22 14:45:48 +0300

    Add CI pipeline

--- jetset/eslint.config.js
+++ jetset/eslint.config.js
@@ -1,13 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      react,
+    },
     languageOptions: {
       globals: {
-        ...globals.browser, // React runs in browser
+        ...globals.browser,
         ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
     rules: {
--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,30 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx"],
-    plugins: {
-      react,
-    },
     languageOptions: {
       globals: {
-        ...globals.browser,
+        ...globals.node, // Node.js runs on server
         ...globals.es2021,
-      },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
       },
     },
     rules: {
